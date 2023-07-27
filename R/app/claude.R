17a9d43d7bc139ed54ad6bdbe1884d5ddf95d720
Will Langdale <will.langdale@trade.gov.uk>
2023-07-27 16:49:11 +0100

    Initial prompt

--- R/app/claude.R
+++ R/app/claude.R
@@ -10,3 +10,18 @@ create_completion_anthropic(
   history = NULL,
   model = model
 )
+
+build_claude_prompt <- function(speeches, ) {
+  paste(speeches, collapse = " ")
+  prompt <- paste0(
+    "What are ", 
+    speakername, 
+    "'s thoughts on ", 
+    user_topic_input, 
+    " below? Please summarise the findings in this format; 5 bullet points, each bullet point to be referenced, the findings to be provided in unbiased language, do not use loaded terminology. Begin the response with \'Here are ",
+    speakername, 
+    "'s views on ", 
+    user_topic_input, 
+    " based on debates in the House of Commons over the past 3 years.\'"
+  )
+}
